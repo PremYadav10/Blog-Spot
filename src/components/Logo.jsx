1cdf3d91c37f2c5406b59a9f22f685f875e7188f
PremYadav10 <yadavprem9993@gmail.com>
2025-09-13 23:39:15 +0530

    fix: Correct default width type in Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Logo({ width = 100 }) {
+function Logo({ width = "100px" }) {
   return (
       <img  width={width} src="src/assets/The Blog Spot.png" alt="img" style={{ margin: "0px", padding: "0px" }}/>
   )
