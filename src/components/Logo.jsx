07369ce8855cbf7b65a6caa06f691b98ad463039
PremYadav10 <yadavprem9993@gmail.com>
2025-09-21 18:37:08 +0530

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({ width = "100px" }) {
   return (
-      <img  width={width} src="src/assets/The Blog Spot.png" alt="img" style={{ margin: "0px", padding: "0px" }}/>
+      <img  width={width} src="./src/assets/The Blog Spot.png" alt="img" style={{ margin: "0px", padding: "0px" }}/>
   )
 }
 
